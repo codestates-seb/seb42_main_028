7b9ff580044224907e9549445767482d40c9d7c4
nayoseb <nayoseb1018@gmail.com>
2023-03-07 10:42:52 +0900

    initial commit

---
+++ src/main/java/com/example/triviewer/TriviewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.triviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/triviewer/TriviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
