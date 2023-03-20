6d30e9b60228097de0a636ab9cfb0c7fa8b4eb76
Lim-Dae-Hoon <ldh960813@gmail.com>
2023-03-20 15:56:26 +0900

    react query 적용

--- client/src/App.js
+++ client/src/App.js
@@ -11,37 +11,49 @@ import Errorpage from './pages/Error/Errorpage.jsx';
 import Header from './components/Header';
 import Reviewlist from './pages/Review/Reviewlist';
 import Card2 from './pages/Review/Reviewlist';
+import Oauth from './pages/Oauth';
 
 import Footer from './components/Footer';
 
 import Layout from './layout/Layout';
 
+import { useIsLoginStore } from './store/loginstore';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const queryClient = new QueryClient();
 
 function App() {
+	const { isLogin, setIsLogin } = useIsLoginStore((state) => state);
+
 	return (
-		<Router>
-			<GlobalStyle />
-			<Header />
-			<Routes>
-				<Route path='/' element={<Landingpage />} />
-				<Route path='/reviewwrite' element={<Reviewwrite />} />
-			</Routes>
-			<Layout>
+		<QueryClientProvider client={queryClient}>
+			<Router>
+				<GlobalStyle />
+				<Header />
 				<Routes>
-					<Route path='/main' element={<Mainpage />} />
-					<Route path='/login' element={<Loginpage />} />
-					<Route path='/signup' element={<Signuppage />} />
-					<Route path='/mypage' element={<Mypage />} />
-					<Route path='/review' element={<Reviewpage />} />
-					<Route path='/admin' element={<Adminpage />} />
-					{/* <Route path='/reviewwrite' element={<Reviewwrite />} /> */}
-					<Route path='404' element={<Errorpage />} />
-					<Route path='test' element={<Card2 />} />
+					<Route path='/' element={<Landingpage />} />
+					<Route path='/reviewwrite' element={<Reviewwrite />} />
 				</Routes>
-			</Layout>
-			<Footer />
-		</Router>
+				<Layout>
+					<Routes>
+						<Route path='/main' element={<Mainpage />} />
+						<Route path='/login' element={<Loginpage />} />
+						<Route path='/signup' element={<Signuppage />} />
+						<Route path='/mypage' element={<Mypage />} />
+						<Route path='/review' element={<Reviewpage />} />
+						<Route path='/admin' element={<Adminpage />} />
+						{/* <Route path='/reviewwrite' element={<Reviewwrite />} /> */}
+						<Route path='/auth/google/callback' element={<Oauth />} />
+						<Route path='404' element={<Errorpage />} />
+						<Route path='test' element={<Card2 />} />
+					</Routes>
+				</Layout>
+				<Footer />
+			</Router>
+		</QueryClientProvider>
 	);
 }
 
