7ccfcc39aafacfb8676e33b257efcff7181ab960
cStatesHwang <jjmes1992@gmail.com>
2023-03-27 10:26:15 +0900

    application.yml, -local.yml 수정

---
+++ src/main/java/com/example/triviewer/s3/S3UpImg.java
@@ -0,0 +1,27 @@
+//package com.example.triviewer.s3;
+//
+//import com.example.triviewer.review.entity.Review;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//
+//@NoArgsConstructor
+//@Getter
+//@Setter //?
+//@Entity
+//public class S3UpImg {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long upImgId;
+//
+//    @Column
+//    private String upImgName;
+//
+//    @Column
+//    private String upImgUrl;
+//
+//    @ManyToOne
+//    private Review review;
+//}
